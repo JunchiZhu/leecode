7df654984bebc9d047deec2bb3564bfbbdd18b97
zhujunch <zhujunch@msu.edu>
2024-07-05 17:02:39 +0800

    day 3 code update

--- day_three/day_three.cpp
+++ day_three/day_three.cpp
@@ -4,21 +4,190 @@
 # include <iostream>
 using namespace std;
 
-class Node{
+class ListNode{
 public:
-    int data;
-    Node* next;
+    int val;
+    ListNode* next;
 
 // parameterized constructor with next node
-    Node (int value, Node *node= nullptr):data(value), next(node){}
-
-// parameterized constructor
-    Node(int value):data(value), next(nullptr){}
+    ListNode (int value, ListNode *node= nullptr):val(value), next(node){}
 
 // default constructor
-    Node():data(0), next(nullptr){}
+    ListNode():val(0), next(nullptr){}
 };
 
-int main(){
 
+ListNode* removeElements(ListNode* head, int val) {
+    while(head!= nullptr and head->val == val){
+        ListNode* temp_node = head;
+        head = head->next;
+        delete temp_node;
+    }
+
+    if (head == nullptr){
+        return nullptr;
+    }
+
+    ListNode* cur = head->next;
+    ListNode* prev = head;
+    while(cur != nullptr){
+        if(cur->val == val){
+            prev->next = cur->next;
+            delete cur;
+            cur = prev->next;
+        } else{
+            prev = prev->next;
+            cur = cur->next;
+        }
+    }
+    return head;
+}
+
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+
+class MyLinkedList {
+public:
+    MyLinkedList(): head(nullptr),size(0) {}
+
+    int get(int index) {
+        if(index>size-1 || index<0){
+            return -1;
+        }
+        ListNode* cur = head;
+        int tracker=0;
+        while (cur!= nullptr){
+            if(tracker==index){
+                return cur->val;
+            }
+            tracker+=1;
+            cur = cur->next;
+        }
+        return -1;
+    }
+
+    void addAtHead(int val) {
+        ListNode* new_node = new ListNode(val);
+        new_node->next = head;
+        head = new_node;
+        size+=1;
+    }
+//    head -> [1] -> [2] -> [3] -> nullptr
+
+    void addAtTail(int val) {
+        ListNode* new_node = new ListNode(val);
+        if(head == nullptr){
+            head=new_node;
+            size+=1;
+            return;
+        }
+        else {
+            ListNode *cur = head;
+            while (cur->next != nullptr) {
+                cur=cur->next;
+            }
+            cur->next=new_node;
+            size+=1;
+        }
+    }
+
+    void addAtIndex(int index, int val) {
+        if(index<0){
+            index = 0;
+        }
+        if(index>size){
+            return;
+        }
+        if(index == 0){
+            addAtHead(val);
+            return;
+        }
+        ListNode* new_node = new ListNode(val);
+        ListNode* cur = head;
+        while(index>1){
+            cur=cur->next;
+            index-=1;
+        }
+
+        new_node->next = cur->next;
+        cur->next = new_node;
+        size+=1;
+    }
+
+    void deleteAtIndex(int index) {
+        if(index<0){
+            index = 0;
+        }
+        if(index>=size){
+            return;
+        }
+        if(index==0){
+            ListNode* temp = head;
+            head = head->next;
+            delete temp;
+            size-=1;
+            return;
+        }
+        ListNode* prev = head;
+        ListNode* cur = head->next;
+        while(index>1){ // 1 2 3  index = 2
+            cur=cur->next;
+            prev=prev->next;
+            index-=1;
+        }
+        prev->next = cur->next;
+        delete cur;
+        size-=1;
+    }
+private:
+    ListNode* head;
+    int size;
+};
+//pre cur
+//     1 2 3
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* cur = head;
+
+    while(cur != nullptr){
+        ListNode* temp = cur->next;
+        cur->next = prev;
+
+        prev=cur;
+        cur=temp;
+    }
+    return prev;
+}
+
+int main() {
+    ListNode* head1 = new ListNode(1);
+    head1->next = new ListNode(2);
+    head1->next->next = new ListNode(6);
+    head1->next->next->next = new ListNode(3);
+
+    cout << "Original List 1: ";
+    printList(head1);
+
+    head1 = removeElements(head1, 6);
+    cout << "Modified List 1 (remove 6): ";
+    printList(head1);
+    delete head1;
+
+    MyLinkedList myLinkedList = *new MyLinkedList();
+    myLinkedList.addAtHead(2);
+    myLinkedList.addAtTail(3);
+    myLinkedList.addAtIndex(1, 2);    // 链表变为 1->2->3
+    myLinkedList.get(1);              // 返回 2
+    myLinkedList.deleteAtIndex(0);    // 现在，链表变为 1->3
+    myLinkedList.get(0);
+
+
+    return 0;
 }
---
+++ day_three/thoughts.md
@@ -0,0 +1,12 @@
+```
+removeElements：
+复习了链表的用法，一开始很陌生但是写完delete后更加明白了如何利用next和prev去跳过想要删除的值了。
+
+MyLinkedList:
+如果链表里还有一个元素，则head->next是null但是head不是null
+灵活运用index去遍历链表
+还要多加练习！
+
+reverseList:
+双指针明白了但是递归还是模糊。
+```
