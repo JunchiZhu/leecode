a5800def5fc3ca25d9ef9843cbf0ef6049617a8f
zhujunch <zhujunch@msu.edu>
2024-09-21 10:57:36 -0400

    back to coding day 16 backtracking

--- day_thirty/day_thirty.cpp
+++ day_thirty/day_thirty.cpp
@@ -4,93 +4,106 @@
 
 #include "../helper.h"
 using namespace std;
+void backtrack(vector<vector<int>> &answer, vector<int> &path, vector<int>& candidates, int target,int sum, int start_index){
+    if(sum>target){
+        return;
+    }
+    if(sum==target){
+        answer.push_back(path);
+        return;
+    }
+    for(int i=start_index;i<candidates.size();++i){
+        sum+= candidates[i];
+        path.push_back(candidates[i]);
+        backtrack(answer,path,candidates,target,sum,i);
+        path.pop_back();
+        sum-= candidates[i];
+    }
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> answer;
+    vector<int> path;
+    int sum=0;
+    backtrack(answer,path,candidates,target,sum,0);
+    return answer;
+}
 
-//vector<vector<int>> combine(int n, int k) {
-//    vector<vector<int>> answer;
-//    for(int i=1;i<n+1;++i){
-//        for(int j=i+1;j<n+1;++j){
-//            vector<int>sub_store;
-//            sub_store.push_back(i);
-//            sub_store.push_back(j);
-//            answer.push_back(sub_store);
-//        }
-//    }
-//    return answer;
-//}
-
-void backtracking(int n, int k, vector<vector<int>> &answer, vector<int> &path, int start_index){
+void backtrack1(vector<vector<int>> &answer, vector<int> &path, int n, int k, int start_index ){
     if(path.size()==k){
         answer.push_back(path);
         return;
     }
-    for(int i=start_index;i<n+1;++i){
+    for(int i=start_index;i<=n-(k-path.size())+1;++i){
         path.push_back(i);
-        backtracking(n,k,answer,path,i+1);
+        backtrack1(answer,path,n,k,i+1);
         path.pop_back();
     }
 }
 vector<vector<int>> combine(int n, int k) {
     vector<vector<int>> answer;
     vector<int> path;
-    backtracking(n,k,answer,path,1);
+    backtrack1(answer,path,n,k,1);
     return answer;
 }
 
-void backtracking3(int k, int target_sum, int sum,int start_index, vector<vector<int>>&answer, vector<int>&path){
-    if(sum>target_sum){
+
+void backtrack2(vector<vector<int>> &answer, vector<int> &path, vector<int>& candidates, int target,int sum, int start_index){
+    if(sum>target){
         return;
     }
-    if(sum==target_sum){
+    if(sum==target){
         answer.push_back(path);
         return;
     }
-    for(int i=start_index;i<10;++i){
-        sum+=i;
+    for(int i=start_index;i<candidates.size()&& sum+candidates[i]<=target;++i){
+        if(start_index<i and candidates[i-1]==candidates[i]){
+            continue;
+        }
+        sum+= candidates[i];
+        path.push_back(candidates[i]);
+        backtrack2(answer,path,candidates,target,sum,i+1);
+        sum-= candidates[i];
+        path.pop_back();
+
+    }
+}
+
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(),candidates.end());
+    vector<vector<int>> answer;
+    vector<int> path;
+    int sum=0;
+    backtrack2(answer,path,candidates,target, sum,0);
+    return answer;
+}
+
+
+
+void backtrack3(vector<vector<int>> &answer, vector<int> &path, int n, int k, int start_index, int sum){
+    if(sum>n){
+        return;
+    }
+    if(path.size()==k and sum==n){
+        answer.push_back(path);
+        return;
+    }
+    for(int i=start_index;i<=9;++i){
         path.push_back(i);
-        backtracking3(k,target_sum,sum,i+1,answer,path);
+        sum+=i;
+        backtrack3(answer,path,n,k,i+1,sum);
         sum-=i;
         path.pop_back();
     }
 }
 
 vector<vector<int>> combinationSum3(int k, int n) {
-    vector<vector<int>>answer;
-    vector<int>path;
-    int sum=0;
-    backtracking3(k,n,sum,1,answer,path);
-    return answer;
-}
-
-
-void backtracking2(vector<int>& candidates, int target, int sum, int startindex, vector<vector<int>> &answer, vector<int> &path) {
-    if (sum > target) {
-        return;
-    }
-    if (sum == target) {
-        answer.push_back(path);
-        return;
-    }
-    for (int i = startindex; i < candidates.size(); ++i) {
-        // 跳过重复元素
-        if (i > startindex && candidates[i] == candidates[i - 1]) {
-            continue;
-        }
-        sum += candidates[i];
-        path.push_back(candidates[i]);
-        backtracking2(candidates, target, sum, i + 1, answer, path);  // i + 1 保证每个元素只能用一次
-        sum -= candidates[i];
-        path.pop_back();
-    }
-}
-
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-    sort(candidates.begin(), candidates.end());  // 排序用于去重
     vector<vector<int>> answer;
     vector<int> path;
-    int sum = 0;
-    backtracking2(candidates, target, sum, 0, answer, path);
+    int sum=0;
+    backtrack3(answer,path,n,k,1, sum);
     return answer;
 }
+
 const string letterMap[10] = {
         "", // 0
         "", // 1
@@ -104,55 +117,76 @@ const string letterMap[10] = {
         "wxyz", // 9
 };
 
-vector<string> result;
-string s;
-void backtracking(const string& digits, int index) {
-    if (index == digits.size()) {
-        result.push_back(s);
+void letter_trackback(vector<string>&answer, string &combine, string &digits, int index) {
+    if(digits.size()==index){
+        answer.push_back(combine);
         return;
     }
-    int digit = digits[index] - '0';        // 将index指向的数字转为int
-    string letters = letterMap[digit];      // 取数字对应的字符集
-    for (int i = 0; i < letters.size(); i++) {
-        s.push_back(letters[i]);            // 处理
-        backtracking(digits, index + 1);    // 递归，注意index+1，一下层要处理下一个数字了
-        s.pop_back();                       // 回溯
+    int digit = digits[index]-'0';
+    string letters = letterMap[digit];
+    for(char letter : letters){
+        combine.push_back(letter);
+        letter_trackback(answer,combine,digits,index+1);
+        combine.pop_back();
     }
 }
+
 vector<string> letterCombinations(string digits) {
-    s.clear();
-    result.clear();
-    if (digits.size() == 0) {
-        return result;
+    vector<string>answer;
+    string combine;
+    if(digits.empty()){
+        return answer;
     }
-    backtracking(digits, 0);
-    return result;
+    letter_trackback(answer,combine,digits,0);
+    return answer;
 }
-//void backtracking(string &digits, vector<string> &answer, string &s, int index){
-//    if(index == digits.size()){
-//        answer.push_back(s);
-//        return;
-//    }
-//    for(int i=0;i<digits.size();++i){
-//
-//    }
-//}
-//
-//vector<string> letterCombinations(string digits) {
-//    vector<string> answer;
-//    string s;
-//    backtracking(digits, answer, s,0);
-//    return answer;
-//
-//}
+
+bool isPalindrome(const string& text, int start, int end){
+    while(start<end){
+        if(text[start]!=text[end]){
+            return false;
+        }
+        start+=1;
+        end-=1;
+    }
+    return true;
+}
+
+void backtrack_text(string &s, vector<vector<string>>&answer, vector<string>&subs, int start){
+    if(start==s.size()){
+        answer.push_back(subs);
+        return;
+    }
+    for(int end=start; end < s.size(); ++end){
+        if(isPalindrome(s, start, end)){
+            subs.push_back(s.substr(start,end-start+1));
+        } else{
+            continue;
+        }
+        backtrack_text(s,answer,subs,end+1);
+        subs.pop_back();
+    }
+}
+vector<vector<string>> partition(string s) {
+    vector<vector<string>>answer;
+    vector<string>subs;
+    backtrack_text(s,answer,subs, 0);
+    return answer;
+}
 
 int main() {
     vector<vector<int>> q1_result = combine(4, 2);
     matrix_printer(q1_result);
 
     vector<vector<int>> q2_result = combinationSum3(3,7);
+
     vector<int> q3_input = {1,2,6,1,5};
     vector<vector<int>> q3_result = combinationSum2(q3_input, 8);
-    string digits="23";
-    vector<string> str = letterCombinations(digits);
+
+    string digits = "234";
+    vector<string> q5_answer = letterCombinations(digits);
+    vector_printer(q5_answer);
+
+    vector<vector<string>> q6_anwers = partition("aab");
+    matrix_printer(q6_anwers);
 }
